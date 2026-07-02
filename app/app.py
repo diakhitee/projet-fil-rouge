8d8c847e1a3ebd2f3a9f941c209f9a7b39050391
Mariama Diakhité <mdiakhite0803@gmail.com>
2026-07-02 08:02:15 -0400

    feat: finalize dashboard and add GitHub Actions

--- app/app.py
+++ app/app.py
@@ -15,15 +15,39 @@ def home():
     # Ajouter des données si la collection est vide
     if collection.count_documents({}) == 0:
         collection.insert_many([
-            {"event": "Connexion VPN réussie"},
-            {"event": "Tentative SSH échouée"},
-            {"event": "Utilisateur admin connecté"}
+            {
+                "time": "09:41",
+                "severity": "HIGH",
+                "source": "SSH",
+                "event": "5 tentatives de connexion échouées"
+            },
+            {
+                "time": "09:45",
+                "severity": "MEDIUM",
+                "source": "VPN",
+                "event": "Connexion depuis une IP inconnue"
+            },
+            {
+                "time": "09:47",
+                "severity": "INFO",
+                "source": "ADMIN",
+                "event": "Connexion administrateur réussie"
+            }
         ])
 
     logs = list(collection.find())
 
-    return render_template("index.html", logs=logs)
+    high = collection.count_documents({"severity": "HIGH"})
+    medium = collection.count_documents({"severity": "MEDIUM"})
+    info = collection.count_documents({"severity": "INFO"})
 
+    return render_template(
+    	"index.html",
+    	logs=logs,
+    	high=high,
+    	medium=medium,
+    	info=info
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
